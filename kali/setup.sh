d331f64d160ba88125b740732c2472d600cf16c2
dekanfrus <dekanfrus@users.noreply.github.com>
2020-03-25 17:36:28 -0500

    Update setup.sh

--- kali/setup.sh
+++ kali/setup.sh
@@ -27,7 +27,7 @@ wdir=`pwd`
 	
 	echo -e "${GRN} [+] Updating System. Please Be Patient.${NC}" >&3
 	apt-get update -qq && apt-get -o Dpkg::Use-Pty=0 install kali-defaults -qq && apt-get -o Dpkg::Use-Pty=0 install kali-linux-full -qq && apt-get upgrade -o Dpkg::Use-Pty=0 -qq && apt-get dist-upgrade -qq -o Dpkg::Use-Pty=0
-	apt-get -o Dpkg::Use-Pty=0 install libsasl2-dev python-dev libldap2-dev libssl-dev cmake python3 xvfb python3-pip python-netaddr python3-dev tesseract-ocr firefox-esr kali-root-login desktop-base kde-plasma-desktop python-pip python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev tilix -qq
+	apt-get -o Dpkg::Use-Pty=0 install libsasl2-dev python-dev libldap2-dev libssl-dev cmake python3 xvfb python3-pip python-netaddr python3-dev tesseract-ocr firefox-esr kali-root-login desktop-base kde-plasma-desktop python-pip python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev tilix xclip -qq
 	
 	# Suppress stdout
 	exec 3>&1 1>/dev/null
@@ -124,6 +124,14 @@ wdir=`pwd`
 		git clone --quiet https://github.com/Tib3rius/AutoRecon
 	echo -e "${GRN}   -- net-creds${NC}" >&3
 		git clone --quiet https://github.com/DanMcInerney/net-creds.git
+	echo -e "${GRN}   -- pspy${NC}" >&3
+		mkdir pspy && cd pspy
+		wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64s
+		wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64
+		wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32s
+		wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32
+		chmod +x pspy*
+		cd /tools/recon
 
 	# Password Tools
 	cd /tools/passwords
